95ce07fb2b05c4cb504514e30f21acee6615a01d
Anil Sharma <100846183+anillkumarsharma@users.noreply.github.com>
2026-01-21 14:09:20 +0530

    code committed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
     
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+  
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import { ConfigModule } from './config/config.module';
+import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    NestConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ConfigModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
-   
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
+  app.enableCors(); // Next.js ke liye required
+  const config = new DocumentBuilder().setTitle('D2D Backend API').setDescription('User management APIs').setVersion('1.0').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+  await app.listen(process.env.PORT || 3001);
+} 
 bootstrap();
